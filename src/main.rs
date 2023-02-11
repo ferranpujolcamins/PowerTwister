0f3263a9dd850a3c94b434893d192ff294dbfe52
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2023-02-11 20:24:38 +0100

    Crappy code to listen to messages from the Twister

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use midir::{MidiInput, MidiInputPort};
+
+const CONTROL_DEVICE_NAME: &str = "Midi Fighter Twister";
 fn main() {
-    println!("Hello, world!");
+    let midi_in = MidiInput::new("midir test input").unwrap();
+
+    println!("Connecting to {}...", CONTROL_DEVICE_NAME);
+    let mut control_device_port: Option<MidiInputPort> = None;
+    for p in midi_in.ports().iter() {
+        if midi_in.port_name(p).unwrap().starts_with(CONTROL_DEVICE_NAME) {
+            control_device_port = Some(p.clone());
+        }
+    }
+
+    let _connection = midi_in.connect(&(control_device_port.unwrap()), "control_input_conection", move |stamp, message, _| {
+        println!("{}: {:?} (len = {})", stamp, message, message.len());
+    }, ());
+
+    loop {};
 }
+// TODO: setup rustfmt
